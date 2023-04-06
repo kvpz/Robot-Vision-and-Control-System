3362a21af3cfdf524529eea154c12216523e58d5
kvpz <unknotted26@gmail.com>
2023-04-06 14:12:05 -0400

    task type updates

--- include/enums/tasktype.hpp
+++ include/enums/tasktype.hpp
@@ -3,7 +3,8 @@
 
 enum TaskType {
         NAVIGATETO, DROPCHIP, GRASP, STACKPED,
-        PATHCORRECTION, POSECORRECTION, ATTRACTIONCOLOR, NA
+        PATHCORRECTION, POSECORRECTION, ATTRACTIONCOLOR, 
+        PICKUPOBJECT, FOLLOWOBJECT, OBJECTSEARCH, NA
 };
 
 static std::string taskTypeToString(TaskType ttype) 
@@ -23,6 +24,12 @@ static std::string taskTypeToString(TaskType ttype)
     return "stackped";
   case ATTRACTIONCOLOR:
     return "attractioncolor";
+  case PICKUPOBJECT:
+    return "pickupobject";
+  case FOLLOWOBJECT:
+    return "followobject";
+  case OBJECTSEARCH:
+    return "objectsearch";
   case NA:
     return "NA";
   default:
@@ -37,6 +44,9 @@ static TaskType taskTypeToEnum(std::string ttype)
     else if(ttype == "posecorrection") return POSECORRECTION;
     else if(ttype == "dropchip") return DROPCHIP;
     else if(ttype == "attractioncolor") return ATTRACTIONCOLOR;
+    else if(ttype == "pickupobject") return PICKUPOBJECT;
+    else if(ttype == "followobject") return FOLLOWOBJECT;
+    else if(ttype == "objectsearch") return OBJECTSEARCH;
     else return NA;
 }
 
--- src/pickupobjecttask.cpp
+++ src/pickupobjecttask.cpp
@@ -1,6 +1,35 @@
 #include "pickupobjecttask.hpp"
 
 PickupObjectTask::PickupObjectTask()
+    : taskType(PICKUPOBJECT)
+{
+    
+}
+
+void PickupObjectTask::notStarted(std::shared_ptr<Map> map, 
+                        std::shared_ptr<Navigator> navigator, 
+                        RobotState& nextRobotState)
+{
+    
+}
+
+void PickupObjectTask::inProgress(std::shared_ptr<Map> map, 
+                        std::shared_ptr<Navigator> navigator, 
+                        RobotState& nextRobotState)
+{
+    
+}
+
+void PickupObjectTask::suspended(std::shared_ptr<Map> map, 
+                        std::shared_ptr<Navigator> navigator, 
+                        RobotState& nextRobotState, TaskType& nextTaskType)
+{
+    
+}
+
+void PickupObjectTask::complete(std::shared_ptr<Map> map, 
+                        std::shared_ptr<Navigator> navigator, 
+                        RobotState& nextRobotState, TaskType& nextTaskType)
 {
     
 }
