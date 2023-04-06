61598f74d3e770e15ad304dac8601a7efc18bb20
kevin <>
2023-04-06 12:23:35 -0400

    new settings header for global variables

---
+++ include/settings.hpp
@@ -0,0 +1,42 @@
+
+#define ORIENTATION_RANGE_TOLERANCE 2.0 // PathCorrection task
+//#define ORIENTATION_RANGE_TOLERANCE 2.0 // Navigator action class
+//#define ORIENTATION_RANGE_TOLERANCE 3.0 // NavigateTo task
+
+// Path correction task settings
+#define PATHCORRECTIONTASK_PRIORITY 4
+
+// Attraction color task settings
+#define ATTRACTIONCOLORTASK_PRIORITY 2
+
+// Drop chip task settings
+#define DEBUG_DROPCHIPTOTASK false
+
+// Navigate To task settings
+#define DEBUG_NAVIGATETOTASK false
+
+// Navigator helper action class settings
+#define NAVDEBUG false
+
+// Pick up object task settings
+#define DEBUG_PICKUPOBJECTTASK false
+#define PICKUPOBJECTTASK_PRIORITY 3
+
+// Pose correction task settings
+#define POSECORRECTIONTASK_PRIORITY 4
+#define POSECORRECTION_DEBUG true
+
+// Robot class settings
+#define ROBOTDEBUG true
+
+// Task class settings
+#define DEBUG_TASK false 
+
+// Task manager class settings
+#define DEBUG_TASKMANAGER true
+
+// Utility class settings
+#define UTILITYDEBUG false
+
+// Main class settings
+#define DEBUG_MAIN false
